6a4df09178bece7497fa7f43bebeb1199f411600
licheng1997 <36041689+licheng1997@users.noreply.github.com>
2020-01-08 20:10:16 +0800

    Add files via upload

---
+++ 未命名1.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c,d;
+	char o;
+	cin>>a>>o>>b;
+	cin>>c>>o>>d;
+	
+	if(b!=c)
+	{
+		cout<<"N"<<endl;
+		} 
+	else
+	{
+		cout<<a<<o<<d<<endl;
+		}
+}
